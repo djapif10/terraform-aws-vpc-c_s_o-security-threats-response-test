a35ebbcb88998dd2a10e4780feb79134d193d0f3
cloudtech-services <djapif@gmail.com>
2022-11-15 17:30:34 -0500

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 0.12.20"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 2.46.0"
+    }
+  }
+}
+
+provider "aws" {
+  region              = var.aws_region
+  allowed_account_ids = var.aws_account_ids
+  profile             = "default"
+}
---
+++ vpc.tf
@@ -0,0 +1,17 @@
+module "sandbox_vpc" {
+  source = "../modules/vpc"
+
+  availability_zones        = var.availability_zones
+  environment               = var.environment
+  
+  # Changed to variable, defined deafult value in terraform.tfvars 
+  vpc_cidr_block            = var.vpc_cidr_block
+  
+  public_subnet_cidr_blocks = var.public_subnet_cidr_blocks
+  
+  
+  # Added private subnets definition 11/08/2022 - ST
+  private_subnet_cidr_blocks = var.private_subnet_cidr_blocks
+}
+
+
